ac867f853d7f6698cf73a69fed39310459ae861b
suniltomar <plussunilsingh@gmail.com>
2026-01-12 16:53:08 +0530

    url updated

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,6 @@ export const CHART_API_BASE_URL =
 // Main Python Backend (Render) for Auth and other features
 export const BACKEND_API_BASE_URL =
   import.meta.env.VITE_BACKEND_API_BASE_URL || "https://api.vegagreeks.com";
-// export const BACKEND_API_BASE_URL =
 //   import.meta.env.VITE_BACKEND_API_BASE_URL || "http://localhost:8000";
 
 export const COMPANY_NAME = "Vega Greeks Calculator";
